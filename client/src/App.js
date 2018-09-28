5362607c25005dce888df75ae41e17c3a6c86e99
Salvatore <salvatore.dep@gmail.comssh-keygen -t rsa -C salvatore.dep@gmail.com>
2018-09-28 14:06:11 -0600

    added background - that's it

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Switch, Route } from "react-router-dom"
-import LogIn from "./components/login/LogIn"
+import LogIn from "./components/home/Home"
 import Game from "./components/game/Game"
 
 const App = () => {
