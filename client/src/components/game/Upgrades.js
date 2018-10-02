4ec15bcb138a575ec576f907c39aa58653054ccc
Salvatore <salvatore.dep@gmail.comssh-keygen -t rsa -C salvatore.dep@gmail.com>
2018-10-02 12:23:38 -0600

    added photos and moved user info bar

--- client/src/components/game/Upgrades.js
+++ client/src/components/game/Upgrades.js
@@ -1,11 +1,20 @@
 import React from "react"
+import coffee from '../../images/coffee.png'
+import desk from '../../images/desk.png'
+import laptop from '../../images/laptop.png'
+import office1 from '../../images/office1.png'
+import office2 from '../../images/office2.png'
+import office3 from '../../images/office3.png'
 
 const Upgrades = (props) => {
     return (
         <div id="upgradeCont">
-            <p onClick={props.coffee}>coffee</p>
-            <p onClick={props.laptop}>laptop</p>
-            <p>job</p>
+            <p onClick={props.coffee}>coffee:  <img src={coffee} alt="" height="100px"/></p>
+            <p onClick={props.laptop}>laptop: <img src={laptop} alt="" height="100px"/></p>
+            <p >desk: <img src={desk} alt="" height="100px"/></p>
+            <p>Office space Job: <img src={office1} alt="" height="100px"/></p>
+            <p>Mediocre Job: <img src={office2} alt="" height="100px"/></p>
+            <p>Big Money: <img src={office3} alt="" height="100px"/></p>
         </div>
     )
 }
